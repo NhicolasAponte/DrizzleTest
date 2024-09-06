72f6e210e30e75657fc91654eda25bbae2329d9f
NhicolasAponte <Nhic.Aponte@gmail.com>
2024-09-06 17:03:33 -0500

    - notes on db connection for vercel driver

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -4,3 +4,27 @@ import postgres from "postgres";
 
 const client = postgres(process.env.DOCKER_POSTGRES_URL as string);
 export const db = drizzle(client, { schema, logger: true});
+
+// https://www.thisdot.co/blog/configure-your-project-with-drizzle-for-local-and-deployed-databases 
+// import { sql } from "@vercel/postgres";
+// import postgres from "postgres";
+// import {
+//   drizzle as VercelDrizzle,
+//   type VercelPgDatabase,
+// } from "drizzle-orm/vercel-postgres";
+// import {
+//   drizzle as LocalDrizzle,
+//   type PostgresJsDatabase,
+// } from "drizzle-orm/postgres-js";
+
+// let db:
+//   | VercelPgDatabase<Record<string, never>>
+//   | PostgresJsDatabase<Record<string, never>>;
+// if (process.env.NODE_ENV === "production") {
+//   db = VercelDrizzle(sql);
+// } else {
+//   const migrationClient = postgres(process.env.POSTGRES_URL as string);
+//   db = LocalDrizzle(migrationClient);
+// }
+
+// export { db };
