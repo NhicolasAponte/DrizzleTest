735003f5e772263c31a9ce705ff1780743417696
NhicolasAponte <Nhic.Aponte@gmail.com>
2024-10-28 14:10:43 -0500

    extracting save procedure to generalized output function for generating data and writing to corresponding directories

--- src/data-generating-functions/generate-users.ts
+++ src/data-generating-functions/generate-users.ts
@@ -2,7 +2,7 @@ import { v4 as uuidv4 } from "uuid";
 import * as fs from "fs";
 import * as path from "path";
 import { generate } from "random-words";
-import { User } from "./type-definitions";
+import { User, UserRole } from "./type-definitions";
 
 // Define the User type based on the UserTable schema
 
@@ -16,7 +16,7 @@ function generateRandomUser(): User {
   const password = Math.random().toString(36).substring(7);
   const roleNum = Math.floor(Math.random() * 10);
   // console.log("roleNum: ", roleNum);
-  const role = roleNum % 2 === 0 ? "USER" : "ADMIN"; // Default role as per the schema
+  const role = roleNum % 2 === 0 ? UserRole.User : UserRole.Admin; // Default role as per the schema
   return { id, email, password, role };
 }
 
@@ -27,32 +27,71 @@ export function generateUsers(numUsers: number, outputDir?: string) {
     users.push(generateRandomUser());
   }
 
-  const fileName = "users";
-  const jsonFilePath = outputDir
-    ? `${outputDir}/${fileName}.json`
-    : `./src/seed-data/${fileName}.json`;
-  const tsFilePath = outputDir
-    ? `${outputDir}/${fileName}.ts`
-    : `./src/seed-data/${fileName}.ts`;
+  let dir = "./src/seed-data";
+  let fileName = "seedUsers";
+  let jsonPath = `${dir}/${fileName}.json`;
+  let tsPath = `${dir}/${fileName}.ts`;
+  let importLine = `import { User } from '../data-generating-functions/type-definitions';\n`;
 
-  const outputDirectory = path.dirname(jsonFilePath);
-  if (!fs.existsSync(outputDirectory)) {
-    fs.mkdirSync(outputDirectory, { recursive: true });
+  saveSeedDataToFiles(users, "User", jsonPath, tsPath, importLine);
+
+  if (outputDir) {
+    jsonPath = `${outputDir}/${fileName}.json`;
+    tsPath = `${outputDir}/${fileName}.ts`;
+    importLine = `import { User, UserRole } from "../definitions/data-model";\n`;
+    saveSeedDataToFiles(users, "User", jsonPath, tsPath, importLine);
   }
+  // // check if directory exists, if not create it
+  // const outputDirectory = path.dirname(jsonPath);
+  // if (!fs.existsSync(outputDirectory)) {
+  //   fs.mkdirSync(outputDirectory, { recursive: true });
+  // }
+  // // write to json file in specified directory
+  // fs.writeFileSync(jsonPath, JSON.stringify(users, null, 2), "utf-8");
+  // console.log(`Generated ${users.length} users and saved to ${jsonPath}`);
 
-  fs.writeFileSync(jsonFilePath, JSON.stringify(users, null, 2), "utf-8");
-  console.log(`Generated ${numUsers} users and saved to ${jsonFilePath}`);
-
-  // Write to TypeScript file
-  // create file content as string
-  const tsContent = `import { User } from '../data-generating-functions/type-definitions';\n\nexport const users: User[] = ${JSON.stringify(
-    users,
-    null,
-    2
-  )};\n`;
-  fs.writeFileSync(tsFilePath, tsContent, "utf-8");
-  console.log(`Generated ${numUsers} users and saved to ${tsFilePath}`);
+  // // write to typescript file
+  // // create file content as string
+  // const tsContent = `import { User } from '../data-generating-functions/type-definitions';\n\nexport const usersSeedArray: User[] = ${JSON.stringify(
+  //   users,
+  //   null,
+  //   2
+  // )};\n`;
+  // fs.writeFileSync(tsPath, tsContent, "utf-8");
+  // console.log(`Generated ${users.length} users and saved to ${tsPath}`);
 }
 
 // Example usage
 //generateUsers(10, './seed-data/users.json');
+
+export function saveSeedDataToFiles(
+  data: any,
+  dataType: string,
+  jsonPath: string,
+  tsPath: string,
+  importLine: string
+) {
+  // check if directory exists, if not create it
+  const outputDirectory = path.dirname(jsonPath);
+  if (!fs.existsSync(outputDirectory)) {
+    fs.mkdirSync(outputDirectory, { recursive: true });
+  }
+  // write to json file in specified directory
+  fs.writeFileSync(jsonPath, JSON.stringify(data, null, 2), "utf-8");
+  console.log(`Generated ${data.length} ${dataType}s and saved to ${jsonPath}`);
+
+  // write to typescript file
+  // create file content as string
+  const tsContent = `${importLine}\nexport const usersSeedArray: User[] = ${JSON.stringify(
+    data,
+    (key, value) => {
+      if (key === "role") {
+        return `UserRole.${value}`;
+      }
+      return value;
+    },
+    2
+  )};\n`;
+  fs.writeFileSync(tsPath, tsContent, "utf-8");
+  console.log(`Generated ${data.length} ${dataType}s and saved to ${tsPath}`);
+}
